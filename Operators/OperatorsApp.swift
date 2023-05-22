133846770e36c3ddbebeb2ab489e470034987630
kanake10 <10kanake@gmail.com>
2023-05-22 15:27:03 +0300

    Initial Commit

---
+++ Operators/OperatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  OperatorsApp.swift
+//  Operators
+//
+//  Created by Ezra Kanake on 22/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct OperatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
